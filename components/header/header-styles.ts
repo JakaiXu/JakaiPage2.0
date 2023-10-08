c6343e15985de804a529372894bbd6ecaf4a2eb2
JakaiXu <105431008+JakaiXu@users.noreply.github.com>
2023-10-08 21:59:25 +1100

    main fix:typo

---
+++ components/header/header-styles.ts
@@ -0,0 +1,25 @@
+export const headerStyles = {
+  container: {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: 2,
+    width: "100%",
+  },
+  innerBox:{
+    maxWidth: "xl",
+    margin: "0 auto",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: "30px 60px",
+    bgcolor: "transparent",
+    zIndex: 1,
+  },
+  text:{
+    fontFamily: '"Poppins", Arial, sans-serif',
+    fontWeight: 900,
+    fontSize: 28,
+  }
+};
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,40 +1,13 @@
-import { Box, Container, IconButton, Typography } from "@mui/material";
+import { Box, IconButton, Typography } from "@mui/material";
 import React from "react";
 import MenuTwoToneIcon from "@mui/icons-material/MenuTwoTone";
 import { Color } from "@/styles/color";
+import { headerStyles } from "./header-styles";
 const Header = () => {
   return (
-    <Box
-      sx={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        right: 0,
-        zIndex: 2,
-        width: "100%",
-      }}
-    >
-      <Box
-        sx={{
-          maxWidth: "xl",
-          margin: "0 auto",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-          padding: "30px 60px",
-          bgcolor: "transparent",
-          zIndex: 1,
-        }}
-      >
-        <Typography
-          sx={{
-            fontFamily: '"Poppins", Arial, sans-serif',
-            fontWeight: 900,
-            fontSize: 28,
-          }}
-        >
-          Jakai.Xu
-        </Typography>
+    <Box sx={headerStyles.container}>
+      <Box sx={headerStyles.innerBox}>
+        <Typography sx={headerStyles.text}>Jakai.Xu</Typography>
         <IconButton>
           <MenuTwoToneIcon sx={{ color: Color.surface.dark }} />
         </IconButton>
