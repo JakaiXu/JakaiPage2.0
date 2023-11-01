fea8f7713238800c3d15e0de3dd29161974ea45c
Jakai Xu <jakaixu@gmail.com>
2023-11-01 20:45:28 +1100

    feat:blog

--- components/blog/index.tsx
+++ components/blog/index.tsx
@@ -1,12 +1,16 @@
-import { Box } from "@mui/material";
+import { Box, Container, Divider } from "@mui/material";
 import React from "react";
-
+import BlogCard from "./blog-card";
+import PageBackgroundPaper from "../utils/page-background-paper";
 
 const Blog = () => {
   return (
-    <Box>
-
-    </Box>
+    <PageBackgroundPaper>
+      <Container>
+        <BlogCard />
+      </Container>
+      <Box sx={{ height: 50 }} />
+    </PageBackgroundPaper>
   );
 };
 
